5cffee6ec9ad4863e31e292c4be60381152baa85
382767553@qq.com <382767553@qq.com>
2017-01-10 17:45:00 +0800

    11

--- controllers/restful/upload.js
+++ controllers/restful/upload.js
@@ -1,40 +1,46 @@
-
+'use strict';
+var qiniu = require('qiniu');
+var config = require(process.cwd() + '/configs/config.js');
+qiniu.conf.ACCESS_KEY = config.ACCESS_KEY;
+qiniu.conf.SECRET_KEY = config.SECRET_KEY;
+var uptoken = new qiniu.rs.PutPolicy(config.Bucket_Name);
 var fs = require('fs'),
     path = require('path'),
     os = require('os');
 var uuid = require('node-uuid');
-var Grid = require('gridfs-stream');
-var parse = require('co-busboy');
-var mongoClient = require(process.cwd() + '/lib/core/mongoClient.js');
+var koaBody = require('koa-body');
+// var Grid = require('gridfs-stream');
+// var parse = require('co-busboy');
+// var mongoClient = require(process.cwd() + '/lib/core/mongoClient.js');
 
 // var tasks = require(process.cwd() + '/lib/core/tasks.js');
 var job = '';
 module.exports = function (app, router) {
-    router.get('/upload/:id', function *(next) {
-        var gfs = Grid(mongoClient.connection.db, mongoClient.mongoose.mongo);
-        var readstream = gfs.createReadStream({
-            filename: this.params.id
-        });
-        this.set('Content-Type', 'image/jpeg');
-        this.body = readstream;
-    });
-    router.post("/upload", function *(next) {
-        var gfs = Grid(mongoClient.connection.db, mongoClient.mongoose.mongo);
-        var prefix = uuid.v1();
-        var parts = parse(this, {
-            autoFields: true
-          })
-        var part
-        var newfilename='';
-        while (part = yield parts) {
-            newfilename =prefix + '_' + part.filename.replace(/[ ]/g,"");
-            if(part.filename.indexOf('.apk')>=0){
-                newfilename = part.filename;
-            }
-            part.pipe(gfs.createWriteStream({ filename: newfilename }));
-        }
-        this.body={code:1,text:newfilename};
-    });
+    // router.get('/upload/:id', function *(next) {
+    //     var gfs = Grid(mongoClient.connection.db, mongoClient.mongoose.mongo);
+    //     var readstream = gfs.createReadStream({
+    //         filename: this.params.id
+    //     });
+    //     this.set('Content-Type', 'image/jpeg');
+    //     this.body = readstream;
+    // });
+    // router.post("/upload", function *(next) {
+    //     var gfs = Grid(mongoClient.connection.db, mongoClient.mongoose.mongo);
+    //     var prefix = uuid.v1();
+    //     var parts = parse(this, {
+    //         autoFields: true
+    //       })
+    //     var part
+    //     var newfilename='';
+    //     while (part = yield parts) {
+    //         newfilename =prefix + '_' + part.filename.replace(/[ ]/g,"");
+    //         if(part.filename.indexOf('.apk')>=0){
+    //             newfilename = part.filename;
+    //         }
+    //         part.pipe(gfs.createWriteStream({ filename: newfilename }));
+    //     }
+    //     this.body={code:1,text:newfilename};
+    // });
     router.get('/release/app', function *(next) {
         var gfs = Grid(mongoClient.connection.db, mongoClient.mongoose.mongo);
         var readstream = gfs.createReadStream({
@@ -53,17 +59,82 @@ module.exports = function (app, router) {
         this.body = readstream;
     });
 
-    router.get('/start/orderFinishBatch', function *(next) {
-        job = tasks.orderFinishBatch();
-        this.body = '开始启动';
-    });
-    router.get('/stop/task', function *(next) {
-        if(!job){
-            this.body = '必须先启动';
-        } else {
-            job.cancel();
-            this.body = '停止执行';
-        }
+    // router.get('/start/orderFinishBatch', function *(next) {
+    //     job = tasks.orderFinishBatch();
+    //     this.body = '开始启动';
+    // });
+    // router.get('/stop/task', function *(next) {
+    //     if(!job){
+    //         this.body = '必须先启动';
+    //     } else {
+    //         job.cancel();
+    //         this.body = '停止执行';
+    //     }
+    // });
+
+
+    router.get('/uptoken', function *(next) {
+        var token = uptoken.token();
+        this.body = {uptoken: token};
     });
 
+
+
+    router.post('/qiniu/upload', koaBody(), function *(next) {
+        var key = uuid.v1();
+        var busboy = new Busboy({headers: this.headers});
+        var filePath = '';
+        var self = this;
+        busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+            filePath = process.cwd() + '/public/upload/' + key;
+            file.pipe(fs.createWriteStream(filePath));
+            file.on('data', function (data) {
+            });
+            file.on('end', function () {
+            });
+        });
+        this.req.pipe(busboy);
+        yield new Promise(function (resolve, reject) {
+            busboy.on('finish', function () {
+                upqiniuFile(filename, filePath, function () {
+                    resolve();
+                });
+            });
+        });
+        self.body = {code: 1, text: key};
+    });
+};
+
+var upqiniuFile = function (key, file, callback) {
+    qiniu.io.putFile(uptoken.token(), key, file, extra, function (err, ret) {
+        if (!err) {
+            // 上传成功， 处理返回值
+
+        } else {
+            console.log(err)
+        }
+        if (fs.existsSync(file)) {
+            fs.unlinkSync(file);
+        }
+        callback();
+    });
+};
+
+var upqiniuBuf = function (key, file, path, callback) {
+    qiniu.io.put(uptoken.token(), key, file, extra, function (err, ret) {
+        if (!err) {
+            // 上传成功， 处理返回值
+            console.log(ret.key, ret.hash);
+
+        } else {
+            // 上传失败， 处理返回代码
+            console.log(err)
+        }
+        if (fs.existsSync(path)) {
+            fs.unlinkSync(path);
+        }
+        if (callback) {
+            callback();
+        }
+    });
 };
---
+++ public/js/qiniupload/uiqiniuupload.js
@@ -0,0 +1,110 @@
+/**
+ * Created by anuode on 2015/3/18.
+ */
+var Domain ='http://ojaeg242y.bkt.clouddn.com/';
+var upFile = {
+    //provider : true,  // 标示上传本地还是七牛服务器
+    //url : 'container', // 本地存储文件操作路径
+    //domain : 'http://qiniu-plupload.qiniudn.com/',// 上传服务器地址
+    //maxSize : '100mb',//文件最大长度
+    /***
+     * @param containerId 上传区域id
+     * @param pickId    上传按钮id
+     * @param displayId   显示区域id
+     * @param url         url
+     * @param maxSize     上传文件大小
+     * @param upComplete  上传结束时函数
+     */
+    upPic: function (filename_type, pickId, displayId, callback) {
+        var uploader = storage.uploader({
+            //headers:{
+            //    Accept:'text/plain'
+            //},
+            runtimes: 'html5,html4',
+            browse_button: pickId,
+            displayId: displayId,
+            //container: containerId,
+            //drop_element: containerId,
+            max_file_size: '100mb',
+            dragdrop: true,
+            uptoken_url: '/uptoken',   // 只用于七牛
+            isimg:true,
+            provider: 'qiniu',
+            filename_type: filename_type,
+            domain: Domain,  // 只用于本地
+            //url: upFileConfig.localurl,   // 只用于本地
+            max_retries: 2,
+            auto_start: true,
+            init: {
+                'FilesAdded': function (up, files) {
+
+                },
+                'BeforeUpload': function (up, file) {
+                    //
+                    //console.log('BeforeUpload');
+                    //var progress = new FileProgress(file, displayId);
+                    //var chunk_size = plupload.parseSize(this.getOption('chunk_size'));
+                    //if (up.runtime === 'html5' && chunk_size) {
+                    //    progress.setChunkProgess(chunk_size);
+                    //}
+                },
+                'UploadProgress': function(up, file) {
+                    var progress = new FileProgress(file, displayId);
+                    var chunk_size = plupload.parseSize(this.getOption('chunk_size'));
+                    progress.setProgress(file.percent + "%", up.total.bytesPerSec, chunk_size);
+
+                },
+                'UploadComplete': function (up, file) {
+
+                },
+
+                'FileUploaded': function(up, file, info) {
+
+                    if (typeof info == "string") {
+                        var progress = new FileProgress(file, displayId);
+                        var progressid =  progress.setComplete(up, info,file);
+                        up.progressid=progressid;
+                        callback(up, info, displayId);
+                    }
+                },
+
+                'Error': function (up, err, errTip) {
+                    alert(errTip);
+                }
+            }
+        });
+    }
+}
+
+
+var storage = new (function () {
+    this.uploader = function (op) {
+        op = op || {};
+        op.chunk_size = '0.5mb';
+        return Qiniu.uploader(op);
+    }
+})();
+
+// A more compact, but less performant, RFC4122v4 solution:
+var uuid = function () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+};
+
+
+var fileUploadInit = function (o) {
+    var fixed = o.fixed || {};    // 固定图片
+    this.addfixed = function (o) {
+        //分组上传  addpic 就是 +
+        var pickId = o.pickId;
+        var displayId = o.displayId;
+        fixed[displayId] = {pickId: pickId, field: o.field, key: o.key};
+        alert(1)
+        if (o.callback) {
+            upFile.upPic(o.filename_type, pickId, displayId, o.callback);
+        }
+    }
+}
+
